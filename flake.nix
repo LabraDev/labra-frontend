3777cda5504857ea05e06d49eaa8167b9a390f10
Gaballa <mgaballa.dev@gmail.com>
2026-03-13 11:37:38 -0700

    add flake
    this is just for me, or anyone else that wants to use nix :3

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+
+	description = "Flake for labra frontend";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+	
+	outputs = { self, nixpkgs, flake-utils }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let pkgs = nixpkgs.legacyPackages.${system};
+			in {
+				devShell = pkgs.mkShell {
+					buildInputs = [ pkgs.nodejs_24 ];
+				
+				shellHook = ''
+					npm i
+				'';
+			};
+		}); 
+}
